031fa303e5d282cb9e26d734018e11713bb8826c
SlayZ121 <slayz9168@gmail.com>
2025-12-15 22:42:48 +0530

    Story and job models written

---
+++ backend/schemas/story.py
@@ -0,0 +1,24 @@
+from typing import List, Optional, dict
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class StoryOptionsSchema(BaseModel):
+	text:str
+	node_id: Optional[int]=None
+
+class StoryNodeBase(BaseModel):
+	content:str
+	is_ending:bool=False
+	is_winning_ending:bool=False
+
+class CompleteStoryNodeResponse(StoryNodeBase):
+	id:int
+	options:List[StoryOptionsSchema]=[]
+
+	class Config:
+		from_attributes = True
+
+
+
+
