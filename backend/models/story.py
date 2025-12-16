41fef04144843660d3b93054a08f28b32e0ef850
SlayZ121 <slayz9168@gmail.com>
2025-12-16 23:08:23 +0530

    Backend complete

--- backend/models/story.py
+++ backend/models/story.py
@@ -3,12 +3,12 @@ from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
 from db.database import Base
-class Story():
+class Story(Base):
 	__tablename__ = "stories"
 	id = Column(Integer, primary_key=True, index=True)
 	title = Column(String, index=True)
 	session_id = Column(String, index=True)
-	created_at=Column(DateTime(timezone=True),server_defaut=func.now())
+	created_at=Column(DateTime(timezone=True),server_default=func.now())
 
 	nodes=relationship("StoryNode",back_populates="story")
 
